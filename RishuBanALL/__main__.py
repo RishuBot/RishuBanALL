03fb33a6bc4325c586b3528168dbc3bbc25c0639
Rishu <vishwakarmarishabh366@gmail.com>
2025-03-06 21:11:31 +0530

    Update __main__.py

--- RishuBanALL/__main__.py
+++ RishuBanALL/__main__.py
@@ -34,7 +34,7 @@ def run_flask():
 # Config vars
 API_ID = int(os.getenv("API_ID","14050586"))
 API_HASH = os.getenv("API_HASH","42a60d9c657b106370c79bb0a8ac560c")
-BOT_TOKEN = os.getenv("BOT_TOKEN","8077840807:AAFjN9-3X-Fle5p3inz_z8xVBDcj_tsxAFM")
+BOT_TOKEN = os.getenv("BOT_TOKEN"," ")
 BOT_USERNAME = os.getenv("BOT_USERNAME","rishu_musicbot")
 MONGO_URI = os.getenv("MONGO_URI","mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(os.getenv("OWNER_ID","5738579437"))

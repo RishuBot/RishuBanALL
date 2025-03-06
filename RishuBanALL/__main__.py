4dc7025a76be903b3fef3f3bfe6671e5073a8800
Rishu <vishwakarmarishabh366@gmail.com>
2025-03-06 20:29:29 +0530

    Update __main__.py

--- RishuBanALL/__main__.py
+++ RishuBanALL/__main__.py
@@ -34,12 +34,12 @@ def run_flask():
 # Config vars
 API_ID = int(os.getenv("API_ID","14050586"))
 API_HASH = os.getenv("API_HASH","42a60d9c657b106370c79bb0a8ac560c")
-BOT_TOKEN = os.getenv("BOT_TOKEN,"8077840807:AAFjN9-3X-Fle5p3inz_z8xVBDcj_tsxAFM")
+BOT_TOKEN = os.getenv("BOT_TOKEN","8077840807:AAFjN9-3X-Fle5p3inz_z8xVBDcj_tsxAFM")
 BOT_USERNAME = os.getenv("BOT_USERNAME","rishu_musicbot")
 MONGO_URI = os.getenv("MONGO_URI","mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority")
 OWNER_ID = int(os.getenv("OWNER_ID","5738579437"))
 FORCE_JOIN1 = os.getenv("FORCE_JOIN1","RishuApi")
-FORCE_JOIN2 = os.getenv("FORCE_JOIN2","https://t.me/+7AUuVrP8F69kYWY1")
+FORCE_JOIN2 = os.getenv("FORCE_JOIN2","bot_maker1437")
 
 # MongoDB setup
 try:
